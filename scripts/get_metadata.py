68bf76bc7306f4278a715b87ec0e9f5171e25949
Ibrahim Dewaswala <ibrahimdewaswala@gmail.com>
2023-05-12 10:39:31 +0530

    cosmetic changes to 2

--- scripts/get_metadata.py
+++ scripts/get_metadata.py
@@ -1,15 +1,27 @@
 import boto3
 import argparse
+import json
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--id","-i",help="EC2 instance id")
-parser.add_argument("--key","-k",help="Key to retrieve value for from metadata")
+parser.add_argument("--key","-k",help="Key to retrieve value for from metadata", default="InstanceId")
+parser.add_argument('--all',help="If all of metadata is needed." , action='store_true')
 args = parser.parse_args()
 
 
-ec2 = boto3.client('ec2')
-response = ec2.describe_instances(InstanceIds=[args.id])
+def convert_datetime_values_to_string(obj):
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            obj[key] = convert_datetime_values_to_string(value)  # Recursive call for nested dictionaries
+    elif isinstance(obj, list):
+        for i in range(len(obj)):
+            obj[i] = convert_datetime_values_to_string(obj[i])  # Recursive call for nested lists
+    elif isinstance(obj, datetime):
+        obj = obj.strftime('%Y-%m-%d %H:%M:%S')  # Convert datetime to string representation
+
+    return obj
 
 def get_value(res, key):
     if type(res) == dict:
@@ -26,4 +38,13 @@ def get_value(res, key):
                 return result
     return None
 
-print(get_value(response,args.key))
+def metadata(args):
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_instances(InstanceIds=[args.id])
+    if args.all:
+        return json.dumps(convert_datetime_values_to_string(response))
+    else:
+        return get_value(response,args.key)
+
+
+print(metadata(args))
